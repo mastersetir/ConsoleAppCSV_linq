14933f686135590b66754922374c6ad31a3d2f5f
h.shafiei <h.shafiei@C138-0010>
2024-08-22 12:10:28 +0330

    14030601-1200

---
+++ ConsoleAppCSV_linq/Cars.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCSV_linq.Cars
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? Name { get; set; }    
+        public double Desplacement { get; set; }
+        public int Cylinders { get; set; }
+        public int City { get; set; }
+        public int Higheay { get; set; }
+        public int Combined { get; set; }
+
+        public  static  Car? ParseFromCSV(string line)
+        { 
+            var car = line.Split(',');
+            if (car.Length == 0)
+                return null;
+            else
+                return  new Car {
+                    Year = int.Parse(car[0]),
+                    Manufacturer = car[1],
+                    Name = car[2],
+                    Desplacement = double.Parse(car[3]),
+                    Cylinders = int.Parse(car[4]),
+                    City = int.Parse(car[5]),
+                    Higheay = int.Parse(car[6]),
+                    Combined = int.Parse(car[7])
+                };
+        }
+
+
+
+
+    } 
+
+}
--- ConsoleAppCSV_linq/Program.cs
+++ ConsoleAppCSV_linq/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace ConsoleAppCSV_linq
+﻿using ConsoleAppCSV_linq.Cars;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleAppCSV_linq
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("CSV (comma-separated values)  linq");
+            var query = ProcessFile("cars.csv");
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+
+
+      private  static List<Car?> ProcessFile(string path)
+        {
+           return  System.IO.File.ReadAllLines(path)
+                            .Skip(1)
+                            .Where(line => line.Length >1)
+                            .Select(Car.ParseFromCSV)
+                            .ToList();             
+         /*  var query = from line in System.IO.File.ReadAllLines(path)
+                        where line.ToString().Length > 1
+                        select Car.ParseFromCSV(line);
+           return query.ToList();
+*/
+
         }
     }
 }
