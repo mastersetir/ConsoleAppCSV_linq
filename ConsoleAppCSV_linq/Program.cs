58884a623b4129ae1fd35ba0a278c80f2e15905b
h.shafiei <h.shafiei@C138-0010>
2024-08-22 10:49:34 +0330

    Add project files.

---
+++ ConsoleAppCSV_linq/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppCSV_linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
